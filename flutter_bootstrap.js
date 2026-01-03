b09dd5be36ba0a4d74e9a9ecfd2761dc99394bf3
SwiftSpotter <ifeanyichukwupeteru@gmail.com>
2026-01-03 11:40:02 +0100

    Deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1206783660"
+    serviceWorkerVersion: "1073494301"
   }
 });
